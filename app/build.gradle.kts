0822d05d9f501c88afa14d8da3ec8dc6f172ca74
Jheider-dev <jheider.mm9@gmail.com>
2025-10-19 16:37:29 -0500

    Pantalla de notificacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.androidx.foundation)
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation(libs.androidx.ui.test.manifest)
